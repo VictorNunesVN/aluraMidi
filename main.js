f62ce589d1c91e90009d403d0c224728ae08d195
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-04-17 09:23:08 -0300

    Update main.js
    alterações

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /* Exemplo 1*/
 document.querySelector('.tecla_pom')
 
-function tocaSom(seletorAudio){
+function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio)
-    if (elemento != null && elemento.localName === 'audio'){
+    if (elemento != null && elemento.localName === 'audio') {
         //localName informa qual propriedade, onde o elemento guarda a informação do nome.
-            elemento.play() 
-        }
-    else{
+        elemento.play()
+    }
+    else {
         console.log('Elemento não encontrado ou seletor inválido')
     }
 }
@@ -20,37 +20,34 @@ const listaDeSons = document.querySelectorAll(".som")
 // Referência variável no JavaScrip ->> Uma variável que receberá um valor novo. São declaradas com LET
 //let contador = 0;
 
-for(let contador = 0; contador < listaDeTeclas.length; contador++){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
 
     // A classList retorna uma lista mostrando o id e a classe ,
     // no caso ele mostra o id que está na posição 1 da lista.  
     const instrumento = tecla.classList[1];
-    
+
     /*Template string ->> `${variável/constante/referencias/contas/...}`*/
     /* No caso, ele concatena uma string com o referencial INSTRUMENTO para formar a string com o id do audio que será tocado. */
     const idAudio = `#som_${instrumento}`;
 
     /**Criando função anônima para receber a função tocaSom. Função anômima ->> function() */
-    tecla.onclick = function(){
+    tecla.onclick = function () {
         //aqui a função tocaSom pega o id de cada audio pra tocar o som específico.
         tocaSom(idAudio)
     }
     //contador = contador + 1;
     // ONKEYDOWN ->> evento de teclado, quando a tecla estiver abaixada.
     // ONKEYUP ->> evento de teclado, quando a tecla estiver levantada
-    tecla.onkeydown = function(evento){
-        if(evento.code === 'Space'){
-        console.log(evento.code === 'Space')
-        // .ADD vai adicionar uma classe a lista de classes do elemento.
-        tecla.classList.add('ativa');
-        }
-        if (evento.code === 'Enter'){
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            // .ADD vai adicionar uma classe a lista de classes do elemento.
             tecla.classList.add('ativa');
         }
-    }
-    tecla.onkeyup = function(){
-        //.REMOVE vai remover uma classe da lista de classes do elemento.
-        tecla.classList.remove('ativa');
+        tecla.onkeyup = function () {
+            //.REMOVE vai remover uma classe da lista de classes do elemento.
+            tecla.classList.remove('ativa');
+        }
     }
 }
